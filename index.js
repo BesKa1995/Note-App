f541dd6a5a5cd92add643ce57501aaa922158921
beso <beskapoladishvili@gmail.com>
2022-04-25 09:53:55 +0400

    added comments

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const addBtn = document.getElementById('add')
 
 
-
 addBtn.addEventListener('click', (e) => {
+
   e.stopPropagation()
+  
   addNewNote('Hello Wrodl!')
 })
 
@@ -19,6 +20,8 @@ function addNewNote(text = '') {
   <div class="main"></div>
   <textarea class="hidden"></textarea>
   `
+
+  //helps not to reach the body element
   note.addEventListener('click', (e) => e.stopPropagation())
 
   const deleteBtn = note.querySelector('.delete')
@@ -26,27 +29,26 @@ function addNewNote(text = '') {
   const textArea = note.querySelector('textarea')
   const main = note.querySelector('.main')
 
+
+  //initialing teaxtArea and main frame of the note
   textArea.value = text
   main.textContent = text
-
   editBtn.addEventListener('click', () => {
     textArea.style.boxShadow = 'inset 2px 4px 5px rgba(0,0,0,.2)'
     textArea.classList.toggle('hidden')
     main.classList.toggle('hidden')
   })
 
-
+  //changing main content of the note by the value of the textarea
   textArea.addEventListener('input', (e) => {
     const { value } = e.target
     main.innerHTML = value
   })
-
+  //switching the view to the main content of the note
   document.body.addEventListener('click', (e) => {
-    textArea.classList.toggle('hidden')
-    main.classList.toggle('hidden')
+    textArea.classList.add('hidden')
+    main.classList.remove('hidden')
   })
-
-
   deleteBtn.addEventListener('click', () => note.remove())
 
   document.body.appendChild(note)
